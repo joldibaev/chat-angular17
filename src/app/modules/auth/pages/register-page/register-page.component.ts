54ca21a80b515152bd3d3681d94981eea7fa0e82
Nurlan Joldibaev <nurlan@payme.uz>
2023-11-14 11:52:18 +0500

    feat: register

--- src/app/modules/auth/pages/register-page/register-page.component.ts
+++ src/app/modules/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,9 @@
-import {Component} from '@angular/core';
+import {ChangeDetectorRef, Component} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AuthApiService} from "../../services/auth-api.service";
+import {AuthService} from "../../../../services/auth.service";
+import {Router} from "@angular/router";
+import {finalize} from "rxjs";
 
 @Component({
   selector: 'app-register-page',
@@ -6,5 +11,39 @@ import {Component} from '@angular/core';
   styleUrl: './register-page.component.scss'
 })
 export class RegisterPageComponent {
+  loading = false;
 
+  formGroup = new FormGroup({
+    email: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'), Validators.required]
+    }),
+    name: new FormControl('', {nonNullable: true, validators: [Validators.required]}),
+    password: new FormControl('', {nonNullable: true, validators: [Validators.required]}),
+  })
+
+  constructor(private apiService: AuthApiService,
+              private authService: AuthService,
+              private router: Router,
+              private changeDetectorRef: ChangeDetectorRef) {
+  }
+
+  submit() {
+    if (this.formGroup.valid) {
+      this.loading = true;
+
+      const {email, name, password} = this.formGroup.getRawValue();
+      this.apiService.register(email, name, password)
+        .pipe(finalize(() => {
+          this.loading = false;
+          this.changeDetectorRef.markForCheck();
+        }))
+        .subscribe({
+          next: (next) => {
+            this.authService.setToken(next.token);
+            void this.router.navigate(['/chat']);
+          },
+        })
+    }
+  }
 }
