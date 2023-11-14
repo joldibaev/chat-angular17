2d90717203cbd8dd30b4f90288d59f5b8ce76fa3
Nurlan Joldibaev <nurlan@payme.uz>
2023-11-14 12:47:39 +0500

    fix: error notyf only on browser

--- src/app/interceptors/error-notyf.interceptor.ts
+++ src/app/interceptors/error-notyf.interceptor.ts
@@ -1,14 +1,18 @@
 import {HttpErrorResponse, HttpInterceptorFn} from '@angular/common/http';
 import {catchError, throwError} from "rxjs";
-import {inject} from "@angular/core";
+import {inject, PLATFORM_ID} from "@angular/core";
 import {NotyfService} from "../services/notyf.service";
+import {isPlatformBrowser} from "@angular/common";
 
 export const errorNotyfInterceptor: HttpInterceptorFn = (req, next) => {
   const notyfService = inject(NotyfService);
+  const platformId = inject(PLATFORM_ID);
 
   return next(req).pipe(catchError((error: HttpErrorResponse) => {
-    const {message} = error.error;
-    notyfService.error(message ?? error.statusText);
+    if (isPlatformBrowser(platformId)) {
+      const {message} = error.error;
+      notyfService.error(message ?? error.statusText);
+    }
 
     return throwError(() => error);
   }));
